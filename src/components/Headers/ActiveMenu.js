5ae2078eeea53f79a015f0e2764c8018b86d35fb
DevvSakib <devvsakib@gmail.com>
2023-10-07 01:43:50 +0600

    useeffect defined

--- src/components/Headers/ActiveMenu.js
+++ src/components/Headers/ActiveMenu.js
@@ -1,7 +1,7 @@
 'use client'
 import Link from "next/link"
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ActiveMenu = ({ path, menu }) => {
     const router = useRouter();
