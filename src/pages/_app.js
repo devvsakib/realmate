ec6d11da6552b480466facae00ea18bbdb188bfc
Dulana Peiris <dulthar@gmail.com>
2023-10-07 10:56:29 +0530

    Added mobile responsive header

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import Footer from '@/components/Footer/Footer'
 import MainHeader from '@/components/Headers/MainHeader'
+import MobileMainHeader from '@/components/Headers/MobileMainHeader'
 import '@/styles/globals.css'
 import '@/styles/tailwind.common.css'
 import Head from 'next/head'
@@ -11,7 +12,12 @@ export default function App({ Component, pageProps }) {
             <link rel="shortcut icon" href="/assets/favicon.svg" type="image/x-icon" />
         </Head>
         <main className='flex flex-col justify-between min-h-screen'>
-            <MainHeader />
+            <div className="lg:hidden">
+                <MobileMainHeader />
+            </div>
+            <div className="hidden lg:block">
+                <MainHeader />
+            </div>
             <Component {...pageProps} />
             <Footer />
         </main>
