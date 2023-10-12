4d776b9b7cbd69a751d0f9cd7e8590d750c7c3e9
harshsbhandari <harshsbhandari@gmail.com>
2023-10-12 13:13:12 +0530

    fixed

--- src/components/Headers/ActiveMenu.js
+++ src/components/Headers/ActiveMenu.js
@@ -3,13 +3,21 @@ import { useEffect, useState } from "react"
 import menu from "../Menus/Menu"
 
 const ActiveMenu = ({ path, menu }) => {
-    const [isActive, setIsActive] = useState(false)
-    // useEffect(() => {
-    //     menu.main.items.map((item) => item.path === path && setIsActive(true))
-    // }, [])
-    return (
-        <Link href={path} className={`${isActive ? "text-primary" : ""}`}>{menu}</Link>
-    )
+  const [isActive, setIsActive] = useState(false)
+  // useEffect(() => {
+  //     menu.main.items.map((item) => item.path === path && setIsActive(true))
+  // }, [])
+  const handleLinkClick = () => {
+    setIsActive(!isActive) // Toggle the isActive state
+  }
+  return (
+    <Link
+      href={path}
+      onClick={handleLinkClick}
+      className={`hover:bg-red-400 p-1 rounded ${isActive ? "bg-red-400 p-1 rounded" : ""}`}>
+      {menu}
+    </Link>
+  )
 }
 
 export default ActiveMenu
--- src/components/Headers/MainHeader.js
+++ src/components/Headers/MainHeader.js
@@ -4,46 +4,53 @@ import menu from "../Menus/Menu"
 import ActiveMenu from "./ActiveMenu"
 
 const MainHeader = () => {
-    return (
-        <header className="py-3 shadow-md">
-            <CommonLayout>
-                <div className="lg:flex justify-between items-center">
-                    <div>
-                        <img className="w-2/4" src="/assets/logo.svg" alt="RealMate" />
-                    </div>
-                    <div>
-                        <ul className="flex gap-6">
-                            {
-                                menu.main.items.map((item, index) => (
-                                    <li>
-                                        <ActiveMenu path={item.path} menu={item.title} />
-                                        
-                                        {/* <Link href={item.path} key={index}>
+  return (
+    <header className="py-3 shadow-md">
+      <CommonLayout>
+        <div className="lg:flex justify-between items-center">
+          <div>
+            <img
+              className="w-2/4"
+              src="/assets/logo.svg"
+              alt="RealMate"
+            />
+          </div>
+          <div>
+            <ul className="flex gap-6">
+              {menu.main.items.map((item, index) => (
+                <li>
+                  <ActiveMenu
+                    path={item.path}
+                    menu={item.title}
+                  />
+
+                  {/* <Link href={item.path} key={index}>
                                             <span className="">{item.title}</span>
                                         </Link> */}
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                    </div>
-                    <div>
-                        <div className="flex items-center gap-2">
-                            <div>
-                                <Link href={"singin"}>
-                                    <span className="">Sign in</span>
-                                </Link>
-                            </div>
-                            <div>
-                                <Link href={"/register"} className="btn-grade">
-                                    <span className="">Register</span>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </CommonLayout>
-        </header>
-    )
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <div className="flex items-center gap-2">
+              <div>
+                <Link href={"singin"}>
+                  <span className="hover:bg-red-400 p-1 rounded">Sign in</span>
+                </Link>
+              </div>
+              <div>
+                <Link
+                  href={"/register"}
+                  className="btn-grade">
+                  <span className="">Register</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </CommonLayout>
+    </header>
+  )
 }
 
 export default MainHeader
