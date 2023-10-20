d706f979eeb4a0f5e872c42fd9ad42a6d4ccfcd4
Abhishek Sood <nyp.abhishek@gmail.com>
2023-10-20 20:41:15 +0530

    slight changes

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -1,16 +1,12 @@
 const Pricing = () => {
   return (
-    
-    <div className="text-center my-10">
-      <h1 className="font-bold text-3xl mb-2">Membership Plans</h1>
-      <h4 className="text-gray-600 text-sm text-center">
-        Upgrade your plan as per your customized requirements. With a paid<br/>
-        membership, you can seamlessly connect with your prospects and get more<br/>
-        responses. Here are some key benefits:<br/>
-      </h4>
-    
-    <div class=" flex flex-row justify-center pt-16">
-        <div class="w-full md:w-1/4 h-[500px] text-white border-2 bg-white rounded-lg shadow relative p-6 md:mr-4 mb-10 md:mb-0">
+    <div>
+    <div class="text-center my-10">
+        <h1 class="font-bold text-3xl mb-2">Membership Plans</h1>
+        <h4 class="text-gray-600 flex justify-center">Upgrade your plan as per your customised requirements. With a paid<br/> membership, you can seamlessly connect with your prospects and get<br/> more responses. Here are some key benefits:</h4>
+    </div>
+    <div class=" flex flex-row justify-center pt-12">
+        <div class="w-full md:w-1/4 h-[500px] text-white border-2 bg-white rounded-lg shadow relative p-10 md:mr-4 mb-10">
             <p class="mt-16 text-gray-400"><span class="font-bold text-pink-500 text-4xl">$0.00</span> /month</p>
             <p class="text-sm text-black font-semibold mt-2">Free Membership</p>    
             <div class=" h-0.5 mr-2 mt-4 bg-gradient-to-r from-pink-500 to-purple-500"></div>
@@ -24,8 +20,8 @@ const Pricing = () => {
                 <li class="my-2 m-4"> See the Likes you've sent in the Last 7 days</li>
             </ul>
         </div>
-          <div class="w-full md:w-1/4 h-[500px] text-white border-2  border-pink-600 bg-white rounded-b-lg shadow relative p-6 md:mr-4 mb-10 md:mb-0">
-            <div class="absolute -inset-x-0.5 -top-9 w-[380px] h-[8%]  bg-purple-900  rounded-t-lg flex justify-center text-sm p-2">20% Off for yearly</div>
+          <div class="w-full md:w-1/4 h-[500px] text-white border-2  border-pink-600 bg-white rounded-b-lg shadow relative p-10 md:mr-4 mb-10 md:mb-0">
+            <div class="absolute -inset-x-0.5 -top-10 w-[380px] h-[9%]  bg-purple-900  rounded-t-lg flex justify-center text-sm p-3">20% Off for yearly</div>
             <p class="mt-16 text-gray-400"><span class="font-bold text-pink-500 text-4xl">$12.00</span> /month</p>
             <p class="text-sm text-black font-semibold mt-2">Premium Membership</p>
             <div class=" h-0.5 mr-2 mt-4 bg-gradient-to-r from-pink-500 to-purple-500"></div>
