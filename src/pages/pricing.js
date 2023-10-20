5c969cfe866c30d11450a8cc05a45ee91f26214b
Abhishek Sood <nyp.abhishek@gmail.com>
2023-10-20 20:20:07 +0530

    small change

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -11,7 +11,7 @@ const Pricing = () => {
     
     <div class=" flex flex-row justify-center pt-16">
         <div class="w-full md:w-1/4 h-[500px] text-white border-2 bg-white rounded-lg shadow relative p-6 md:mr-4 mb-10 md:mb-0">
-            <p class="mt-16 text-gray-400"><span class="font-bold text-pink-500 text-4xl">$0.00</span> /Month</p>
+            <p class="mt-16 text-gray-400"><span class="font-bold text-pink-500 text-4xl">$0.00</span> /month</p>
             <p class="text-sm text-black font-semibold mt-2">Free Membership</p>    
             <div class=" h-0.5 mr-2 mt-4 bg-gradient-to-r from-pink-500 to-purple-500"></div>
             <ul class="text-sm list-disc text-black mt-8 pr-7">
@@ -26,8 +26,8 @@ const Pricing = () => {
         </div>
           <div class="w-full md:w-1/4 h-[500px] text-white border-2  border-pink-600 bg-white rounded-b-lg shadow relative p-6 md:mr-4 mb-10 md:mb-0">
             <div class="absolute -inset-x-0.5 -top-9 w-[380px] h-[8%]  bg-purple-900  rounded-t-lg flex justify-center text-sm p-2">20% Off for yearly</div>
-            <p class="mt-16 text-gray-400"><span class="font-bold text-pink-500 text-4xl">$0.00</span> /Month</p>
-            <p class="text-sm text-black font-semibold mt-2">Free Membership</p>
+            <p class="mt-16 text-gray-400"><span class="font-bold text-pink-500 text-4xl">$12.00</span> /month</p>
+            <p class="text-sm text-black font-semibold mt-2">Premium Membership</p>
             <div class=" h-0.5 mr-2 mt-4 bg-gradient-to-r from-pink-500 to-purple-500"></div>
             <ul class="text-sm list-disc text-black mt-8 pr-7">
                 <li class="my-2 m-4"> Match, Chat & Meet</li>
